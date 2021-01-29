ecb8b8ff0ec64ab2a3fed4ff50e0c7efb48840a9
unknown <ducmanhtb2802@gmail.com>
2021-01-29 16:24:58 +0700

    Bai 2

--- src/DaySoFibolaci.java
+++ src/DaySoFibolaci.java
@@ -5,9 +5,9 @@ public class DaySoFibolaci {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Nhập vào n: ");
         int n = scanner.nextInt();
-        if (n < 0) {
+        if (n <= 0) {
             System.out.println("Số đã cho không là số nguyên dương");
-        } else if (n == 0 || n == 1) {
+        } else if (n == 1) {
             System.out.println("Kết quả: " + n);
         } else {
             int f0 = 0, f1 = 1, fn = 1;
